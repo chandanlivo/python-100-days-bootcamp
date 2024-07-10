e6d75f400c56423d59186c1697955ac548f92c2d
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-07-10 22:39:49 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,15 @@
+num1 = '22'
+num2 = '23'
+print(num1 + num2) # 2223
+print(int(num1) + int(num2)) # 45
+print(float(num1) + float(num2)) # 45.0
+
+num_char = len(input("What is your name: "))
+new_char= str(num_char)  # int to string
+print("Your name has "+new_char+ " characters")
+
+a= int(22.2) #float to int 
+print(a)
+print(type(a))
+
+print(70 + float("22.2")) #string to float
