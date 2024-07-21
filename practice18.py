43cc438b18ec3f538f6f7f39172867f33e0a15e5
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-07-21 12:54:04 +0530

    Add files via upload

---
+++ practice18.py
@@ -0,0 +1,6 @@
+# Fuctions with multiple parameters
+def greet(name, location):
+    print(f"My name is {name}, I'm from {location}")
+#greet("ABD", "pretoria")  Positional arguments
+greet(location= "Bengaluru", name= "ABD")  #keyword arguments
+
